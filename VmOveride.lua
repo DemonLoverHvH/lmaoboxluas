952e9c83ff38712a795e2fa242d4fb63201ebc93
Trophy <51270049+DemonLoverHvH@users.noreply.github.com>
2022-04-24 00:56:24 +0200

    weapon sway

--- VmOveride.lua
+++ VmOveride.lua
@@ -1,7 +1,6 @@
 --[[
-    Viewmodel Override For Lmaobox
-    Lib Author: LNX (github.com/lnx00)
-    Author: Trophy github.com/DemonLoverHvH
+    Misc Tools for Lmaobox
+    Author: LNX (github.com/lnx00)
 ]]
 
 local menuLoaded, MenuLib = pcall(require, "Menu")
@@ -16,6 +15,7 @@ local Enable = menu:AddComponent(MenuLib.Checkbox("Enable", false))
 local Viewmodelx = menu:AddComponent(MenuLib.Slider("Viewmodel X", -100, 100, 0))
 local Viewmodely = menu:AddComponent(MenuLib.Slider("Viewmodel Y", -100, 100, 0))
 local Viewmodelz = menu:AddComponent(MenuLib.Slider("Viewmodel Z", -100, 100, 0))
+--local WpnSwayScale = menu:AddComponent(MenuLib.Slider("Weapon Sway", 0, 10, 0))
 
 
 
@@ -26,8 +26,15 @@ local function CreateMove(pCmd)
         local y = Viewmodelz:GetValue()
         local z = Viewmodely:GetValue()
         client.RemoveConVarProtection( "tf_viewmodels_offset_override")
+        client.RemoveConVarProtection( "cl_wpn_sway_interp")
+        client.RemoveConVarProtection("cl_wpn_sway_scale") 
+
         client.SetConVar( "tf_viewmodels_offset_override", x .. " " .. y .. " " .. z )
- 
+
+        local sway = WpnSwayScale:GetValue() -- couldn't get it to work :/ if you do please make pull request
+        client.SetConVar("cl_wpn_sway_scale", 7) -- change numbers to switch sway ammount i just like it like this
+        client.SetConVar("cl_wpn_sway_interp", 5)
+
     end
 end
 
