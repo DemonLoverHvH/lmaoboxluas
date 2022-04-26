207fceb9ab4be5e4deb5ec0eb9d79db4e4c55268
Trophy <51270049+DemonLoverHvH@users.noreply.github.com>
2022-04-26 17:49:12 +0200

    hotfix

--- VmOveride.lua
+++ VmOveride.lua
@@ -9,7 +9,7 @@ assert(MenuLib.Version >= 1.35, "MenuLib version is too old, please update it!")
 
 local ltExtendFreeze = 0
 
--- Menu
+-- Menu frontend
 local menu = MenuLib.Create("Better Viewmodel", MenuFlags.AutoSize)
 local Enable = menu:AddComponent(MenuLib.Checkbox("Enable", false))
 local Viewmodelx = menu:AddComponent(MenuLib.Slider("Viewmodel X", -100, 100, 0))
@@ -17,21 +17,23 @@ local Viewmodely = menu:AddComponent(MenuLib.Slider("Viewmodel Y", -100, 100, 0)
 local Viewmodelz = menu:AddComponent(MenuLib.Slider("Viewmodel Z", -100, 100, 0))
 --local WpnSwayScale = menu:AddComponent(MenuLib.Slider("Weapon Sway", 0, 10, 0))
 
+--remove cvar protection
+client.RemoveConVarProtection( "tf_viewmodels_offset_override")
+client.RemoveConVarProtection( "cl_wpn_sway_interp")
+client.RemoveConVarProtection("cl_wpn_sway_scale") 
 
-
+--menu backend
 local function CreateMove(pCmd)
     if Enable:GetValue() == true then
 
         local x = Viewmodelx:GetValue()
         local y = Viewmodelz:GetValue()
         local z = Viewmodely:GetValue()
-        client.RemoveConVarProtection( "tf_viewmodels_offset_override")
-        client.RemoveConVarProtection( "cl_wpn_sway_interp")
-        client.RemoveConVarProtection("cl_wpn_sway_scale") 
+
 
         client.SetConVar( "tf_viewmodels_offset_override", x .. " " .. y .. " " .. z )
 
-        local sway = WpnSwayScale:GetValue() -- couldn't get it to work :/ if you do please make pull request
+        -- local sway = WpnSwayScale:GetValue() -- couldn't get it to work :/ if you do please make pull request
         client.SetConVar("cl_wpn_sway_scale", 7) -- change numbers to switch sway ammount i just like it like this
         client.SetConVar("cl_wpn_sway_interp", 5)
 
